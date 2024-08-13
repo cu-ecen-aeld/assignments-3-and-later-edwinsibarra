b91f754ab23ced74baaadb2ffd89530f7f678623
Edwins Ibarra <edwins.ibarra@icloud.com>
2024-08-13 11:49:05 +0200

    second commit

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -12,11 +12,11 @@
 *   3) Use unity assertion TEST_ASSERT_EQUAL_STRING_MESSAGE to verify the two strings are equal.  See
 *       the [unity assertion reference](https://github.com/ThrowTheSwitch/Unity/blob/master/docs/UnityAssertionsReference.md)
 */
+
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
-}
+    
+    const char *str1 = my_username();
+    char *str2 = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(str1, str2, "username does not match");
+}   
