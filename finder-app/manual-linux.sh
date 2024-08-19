7232b5aecf16fabfa7363022a651b3fcb7325218
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-08-19 16:48:49 +0200

    Building steps_1

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    QEMU virt
     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-mrpoper
     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-defconfig
     make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-all
