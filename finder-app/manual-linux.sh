af71216369626cd94cbe6c82106a2078befbb2aa
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-08-20 18:52:19 +0200

    Directory names added

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -59,8 +59,8 @@ fi
 # TODO: Create necessary base directories
 mkdir -p "${OUTDIR}/rootfs"
 cd "${OUTDIR}/rootfs"
-mkdir -p bin dev etc hom lib lib64 proc sbin tmp usr var
-mkdir -p usr/bin usr/lin usr/sbin
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
