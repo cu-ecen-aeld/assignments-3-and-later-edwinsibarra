7edecaabad3d84b8dac38aa0f598959cf2ec66f4
vboxuser <edwins.ibarra@icloud.com>
2024-08-21 23:46:34 +0200

    Everything, but dynamically allocated struct

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,7 +15,15 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    //waiting
+    sleep(&thread_func_args->wait_to_obtain_ms);
+    //obtaining
+    pthread_mutex_lock(&thread_func_args->mutex);
+    //waiting
+    sleep(&thread_func_args->wait_to_release_ms);
+    //releasing
+    pthread_mutex_unlock(&thread_func_args->mutex);
 }
 
 
@@ -28,6 +37,15 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+     return false;
+     struct thread_data tinfo = {wait_to_obtain_ms, wait_to_release_ms, *mutex, false};
+     int rc = pthread_create(&thread,
+                            NULL,
+                            threadfunc,
+                            &tinfo);
+    if (*thread){
+        tinfo.thread_complete_success = true;
+        return true;
+    }
 }
 
