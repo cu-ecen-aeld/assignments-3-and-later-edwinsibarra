64703ee0c49ba0600b62ba0c1a3d12cd37e08630
vboxuser <edwins.ibarra@icloud.com>
2024-08-22 12:26:30 +0200

    Excercise 3.2 is in a commit down below; changing seconds into miliseconds, correction

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -21,7 +21,7 @@ void* threadfunc(void* thread_param)
     //obtaining
     pthread_mutex_lock(&thread_func_args->mutex);
     //waiting
-    sleep(thread_func_args->wait_to_release_ms) * 1000;
+    sleep(thread_func_args->wait_to_release_ms * 1000);
     //releasing
     pthread_mutex_unlock(&thread_func_args->mutex);
     thread_func_args->thread_complete_success = true;
