07aec8b65d0421f0552fdca0c3ad576b3f40f156
vboxuser <edwins.ibarra@icloud.com>
2024-09-09 02:17:10 +0200

    Updates needed for assignment 4.2, little refactor

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -12,17 +12,17 @@ username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
-    echo "Using default value ${WRITESTR} for string to write"
-    if [ $# -lt 1 ]
-    then
-        echo "Using default value ${NUMFILES} for number of files to write"
-    else
-        NUMFILES=$1
-    fi    
+	echo "Using default value ${WRITESTR} for string to write"
+	if [ $# -lt 1 ]
+	then
+		echo "Using default value ${NUMFILES} for number of files to write"
+	else
+		NUMFILES=$1
+	fi	
 else
-    NUMFILES=$1
-    WRITESTR=$2
-    WRITEDIR=/tmp/aeld-data/$3
+	NUMFILES=$1
+	WRITESTR=$2
+	WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -34,26 +34,25 @@ rm -rf "${WRITEDIR}"
 # create $WRITEDIR if not assignment1
 assignment=`cat /etc/finder-app/conf/assignment.txt`
 
-if [ $assignment != 'assignment4' ]
+if [ $assignment = 'assignment4' ]
 then
-    mkdir -p "$WRITEDIR"
+	mkdir -p "$WRITEDIR"
 
-    if [ -d "$WRITEDIR" ]
-    then
-        echo "$WRITEDIR created"
-    else
-        exit 1
-    fi
+	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+	#The quotes signify that the entire string in WRITEDIR is a single string.
+	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
+	if [ -d "$WRITEDIR" ]
+	then
+		echo "$WRITEDIR created"
+	else
+		exit 1
+	fi
 fi
-
-# Clean previous build artifacts and compile the writer application
-echo "Removing the old writer utility and compiling as a native application"
-make clean
-make writer
+#echo "Removing the old writer utility and compiling as a native application"
 
 for i in $( seq 1 $NUMFILES)
 do
-    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
@@ -65,9 +64,9 @@ rm -rf /tmp/aeld-data
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-    echo "success"
-    exit 0
+	echo "success"
+	exit 0
 else
-    echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-    exit 1
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	exit 1
 fi
